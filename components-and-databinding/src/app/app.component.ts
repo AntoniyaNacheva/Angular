5ab73d10204abe20171a5a8389343695bbeda7ae
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-01-27 20:29:45 +0200

    Binding to Custom Properties (@Input())

--- components-and-databinding/src/app/app.component.ts
+++ components-and-databinding/src/app/app.component.ts
@@ -6,23 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent,
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent,
-    });
-  }
+  serverElements = [
+    { type: 'server', name: 'TestServer', content: 'Just a test!' },
+  ];
 }
