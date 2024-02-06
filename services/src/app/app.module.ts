15198846b1f39606810ab6c39885d310846e96c4
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-02-06 11:50:16 +0200

    @Injectable() (Services into Services)

--- services/src/app/app.module.ts
+++ services/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 import { AccountsService } from './accounts.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [AppComponent, AccountComponent, NewAccountComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [AccountsService],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { LoggingService } from '../logging.service';
 import { AccountsService } from '../accounts.service';
 
@@ -6,7 +7,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrl: './new-account.component.css',
-  providers: [LoggingService],
+  // providers: [LoggingService],
 })
 export class NewAccountComponent {
   constructor(
@@ -16,6 +17,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
