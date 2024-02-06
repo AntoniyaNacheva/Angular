331ce6ecde793083ecf8ac69c9c16462d184f1d0
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-02-06 14:03:00 +0200

    Assignment Services

---
+++ assignment-services/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { UserService } from './users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  providers: [UserService],
+})
+export class AppComponent {}
